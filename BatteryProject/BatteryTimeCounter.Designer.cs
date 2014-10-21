a2bc43118b303dd0fa686df644b4f75e3d70352f
Mi10s <milos.keza@gmail.com>
2014-10-21 12:38:00 +0200

    Initial working version
    This app helps you track how long your laptop is on battery

---
+++ BatteryProject/BatteryTimeCounter.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace BatteryProject
+{
+    partial class BatteryTimeCounter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BatteryTimeCounter));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.meniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contactToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblRemainingTime = new System.Windows.Forms.Label();
+            this.lblRemainingTimeStatic = new System.Windows.Forms.Label();
+            this.lblProcent = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblProcentStatic = new System.Windows.Forms.Label();
+            this.lblElapsedTime = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 25);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(67, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Charging";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(13, 55);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "On Battery";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.meniToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // meniToolStripMenuItem
+            // 
+            this.meniToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.meniToolStripMenuItem.Name = "meniToolStripMenuItem";
+            this.meniToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.meniToolStripMenuItem.Text = "Menu";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.contactToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // contactToolStripMenuItem
+            // 
+            this.contactToolStripMenuItem.Name = "contactToolStripMenuItem";
+            this.contactToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.contactToolStripMenuItem.Text = "Contact";
+            this.contactToolStripMenuItem.Click += new System.EventHandler(this.contactToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblRemainingTime);
+            this.groupBox1.Controls.Add(this.lblRemainingTimeStatic);
+            this.groupBox1.Controls.Add(this.lblProcent);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.lblProcentStatic);
+            this.groupBox1.Controls.Add(this.lblElapsedTime);
+            this.groupBox1.Location = new System.Drawing.Point(12, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 120);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Battery info";
+            // 
+            // lblRemainingTime
+            // 
+            this.lblRemainingTime.AutoSize = true;
+            this.lblRemainingTime.Location = new System.Drawing.Point(98, 93);
+            this.lblRemainingTime.Name = "lblRemainingTime";
+            this.lblRemainingTime.Size = new System.Drawing.Size(40, 13);
+            this.lblRemainingTime.TabIndex = 11;
+            this.lblRemainingTime.Text = "00:00h";
+            // 
+            // lblRemainingTimeStatic
+            // 
+            this.lblRemainingTimeStatic.AutoSize = true;
+            this.lblRemainingTimeStatic.Location = new System.Drawing.Point(10, 93);
+            this.lblRemainingTimeStatic.Name = "lblRemainingTimeStatic";
+            this.lblRemainingTimeStatic.Size = new System.Drawing.Size(82, 13);
+            this.lblRemainingTimeStatic.TabIndex = 10;
+            this.lblRemainingTimeStatic.Text = "Reamining time:";
+            // 
+            // lblProcent
+            // 
+            this.lblProcent.AutoSize = true;
+            this.lblProcent.Location = new System.Drawing.Point(226, 63);
+            this.lblProcent.Name = "lblProcent";
+            this.lblProcent.Size = new System.Drawing.Size(21, 13);
+            this.lblProcent.TabIndex = 9;
+            this.lblProcent.Text = "0%";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(63, 63);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(157, 13);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // lblProcentStatic
+            // 
+            this.lblProcentStatic.AutoSize = true;
+            this.lblProcentStatic.Location = new System.Drawing.Point(10, 63);
+            this.lblProcentStatic.Name = "lblProcentStatic";
+            this.lblProcentStatic.Size = new System.Drawing.Size(47, 13);
+            this.lblProcentStatic.TabIndex = 7;
+            this.lblProcentStatic.Text = "Procent:";
+            // 
+            // lblElapsedTime
+            // 
+            this.lblElapsedTime.AutoSize = true;
+            this.lblElapsedTime.Location = new System.Drawing.Point(10, 36);
+            this.lblElapsedTime.Name = "lblElapsedTime";
+            this.lblElapsedTime.Size = new System.Drawing.Size(53, 13);
+            this.lblElapsedTime.TabIndex = 6;
+            this.lblElapsedTime.Text = "RunTime:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 168);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(260, 82);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Charging state";
+            // 
+            // BatteryTimeCounter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "BatteryTimeCounter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BatteryTimeCounter";
+            this.Load += new System.EventHandler(this.BatteryTimeCounter_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+            
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem meniToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contactToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblRemainingTime;
+        private System.Windows.Forms.Label lblRemainingTimeStatic;
+        private System.Windows.Forms.Label lblProcent;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblProcentStatic;
+        private System.Windows.Forms.Label lblElapsedTime;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ BatteryProject/BatteryTimeCounter.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace BatteryProject
+{
+    public partial class BatteryTimeCounter : Form
+    {
+        
+        private Stopwatch stopWatch;
+        
+        public BatteryTimeCounter()
+        {
+            InitializeComponent();
+            stopWatch = new Stopwatch();
+           
+            timer1.Start();
+
+        }
+
+        void OnPowerChange(Object sender, PowerModeChangedEventArgs e) {
+
+            if (e.Mode == PowerModes.Suspend)
+            {
+                timer1.Stop();
+                MessageBox.Show("System je u standby");
+            }
+            else if (e.Mode == PowerModes.Resume) timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+             SystemEvents.PowerModeChanged += OnPowerChange;
+            
+            radioButton1.Checked = System.Windows.Forms.SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Online;
+            radioButton2.Checked = System.Windows.Forms.SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Offline;
+
+            if (System.Windows.Forms.SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Offline)
+            {
+                stopWatch.Start();
+            }
+            else
+            {
+                stopWatch.Stop();
+            }
+            
+            
+            TimeSpan ts = stopWatch.Elapsed;
+
+            lblElapsedTime.Text = "RunTime: " + String.Format("{0:00}:{1:00}:{2:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10); ;
+
+            lblProcent.Text = "" + (SystemInformation.PowerStatus.BatteryLifePercent * 100) + "%";
+            progressBar1.Value = (int)(SystemInformation.PowerStatus.BatteryLifePercent * 100);
+            lblRemainingTime.Text = "" + (SystemInformation.PowerStatus.BatteryLifeRemaining/60)+"min.";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();     
+        }
+
+        private void contactToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new AboutMe().ShowDialog();
+        }
+
+        private void BatteryTimeCounter_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+
+
+    }
+}
